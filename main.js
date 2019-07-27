930dbcdb3ce93236e05754c70017fed0965310b9
PWCB <pwcbrock@gmail.com>
2019-07-26 22:03:26 -0700

    a

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ side = document.getElementsByClassName("side")[0],
 footer =  document.getElementsByClassName("footer")[0],
 modal = document.getElementsByClassName("modal")[0],
 modalContent = document.getElementsByClassName("modalContent")[0],
-videoFrame = document.getElementById('videoFrameId'),
+contentFrame = document.getElementById('contentFrameId'),
 modalOverlay = document.getElementsByClassName("modalOverlay")[0],
 fakeScroll = document.getElementsByClassName("fakeScroll")[0],
 modalInfo =  document.getElementsByClassName("modalInfo")[0],
@@ -36,6 +36,31 @@ musiclnk = document.getElementById('music');
 var btn = new Array(itmimax);
 btn = document.getElementsByClassName("itmbtn");
 
+var bnr = new Array(itmimax);
+bnr = document.getElementsByClassName("itmTextBack");
+for(i = 0; i < bnr.length; i++){
+	var col = Math.floor(Math.random() * 6);
+	switch(col){
+		case 0:
+			bnr[i].style.backgroundColor = "rgba(255,0,0,1.0)";
+			break;
+		case 1:
+			bnr[i].style.backgroundColor = "rgba(0,255,100,1.0)";
+			break;
+		case 2:
+			bnr[i].style.backgroundColor = "rgba(0,100,255,1.0)";
+			break;
+		case 3:
+			bnr[i].style.backgroundColor = "rgba(255,220,0,1.0)";
+			break;
+		case 4:
+			bnr[i].style.backgroundColor = "rgba(255,100,0,1.0)";
+			break;
+	}
+	//bnr[i].style.backgroundColor = "rgba(0,100,255,1.0)";
+	bnr[i].style.backgroundColor = "rgba(255,220,0,1.0)";
+}
+
 var btnDiv = new Array(itmimax);
 btnDiv = document.getElementsByClassName("itmDiv");
 
@@ -49,6 +74,8 @@ function clearGrid(){
 		grid[i] = -1;
 	}
 }
+
+
 
 function categorizeGrid(){
 	var ii = 0;
@@ -150,6 +177,13 @@ window.mobileCheck = function(){
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
+
+window.onresize = function(){
+	dispBtns = 0;
+	colms = (window.innerWidth-btnsize*2)/btnsize;
+	rows = Math.ceil(realitms/colms);
+	positionGrid();
+}
 
 function insertText(className, txt){
     document.getElementsByClassName(className)[0].innerHTML = txt;
@@ -267,8 +301,8 @@ arrowr.onclick = function(){
 //exit
 closer.onclick = function(){
 	modal.style.display = "none";
-	videoFrame.src = "none";
-	//videoFrame.contentWindow.postMessage('{"event":"command","func":"' + 'stopvideo' + '","args":""}', '*');
+	contentFrame.src = "none";
+	//contentFrame.contentWindow.postMessage('{"event":"command","func":"' + 'stopvideo' + '","args":""}', '*');
 	itmi = 0;
 	scrollTgt = 100;
 }
@@ -276,7 +310,7 @@ closer.onclick = function(){
 info.onclick = function(){
 	if (scrollTgt > 90){
 		scrollTgt = 80;
-		scrollTgt = clamp(scrollTgt, 0 - ($(modalInfo).outerHeight(true) - $(videoFrame).outerHeight(true))/$(videoFrame).outerHeight(true)*100, 100);
+		scrollTgt = clamp(scrollTgt, 0 - ($(modalInfo).outerHeight(true) - $(contentFrame).outerHeight(true))/$(contentFrame).outerHeight(true)*100, 100);
 	}else{
 		scrollTgt = 100;
 	}
@@ -285,8 +319,8 @@ info.onclick = function(){
 modal.onclick = function(event){
   if (event.target == modal && event.target != arrowl && event.target != arrowr && event.target != info && iaOn == 0){
     modal.style.display = "none";
-	videoFrame.src = "none";
-	//videoFrame.contentWindow.postMessage('{"event":"command","func":"' + 'stopvideo' + '","args":""}', '*');
+	contentFrame.src = "none";
+	//contentFrame.contentWindow.postMessage('{"event":"command","func":"' + 'stopvideo' + '","args":""}', '*');
 	itmi = 0;
 	scrollTgt = 100;
   }
@@ -382,63 +416,84 @@ function step(){
 		switch(itmi) {
 			case 1:
 				modal.style.display = "block";
-				videoFrame.src = "egg.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.src = "egg.mp4";
 				title = "Good Morning";
 				txt = "<br/> Song: Plantasia by Mort Garson";
 				break;
 			case 2:
 				modal.style.display = "block";
-				videoFrame.src = "fall.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.src = "fall.mp4";
 				title = "Fall";
 				txt = "<br/> Song: Invisible Grid by Small Black";
 				break;
 			case 3:
 				modal.style.display = "block";
-				videoFrame.src = "raccoon.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.src = "raccoon.mp4";
 				title = "Feelin Gud";
 				txt = "<br/> Song: Flowers On A Wall by Daisuke Tanabe";
 				break;
 			case 4:
 				modal.style.display = "block";
-				videoFrame.src = "tubeguy.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.src = "tubeguy.mp4";
 				title = "Tubeguy";
 				txt = "<br/> Song: Tranquility Path by Windows69";
 				break;
 			case 5:
 				modal.style.display = "block";
-				videoFrame.src = "roads.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.src = "roads.mp4";
 				title = "";
 				txt = "Song: Bjork Stonemilker remix by Forest Swords";
 				break;
 			case 6:
 				modal.style.display = "block";
-				videoFrame.src = "hunters.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.src = "hunters.mp4";
 				title = "looking for parking";
 				txt = "<br/> Song: Ashtray Wasp by Burial";
 				break;
 			case 7:
 				modal.style.display = "block";
-				videoFrame.src = "vhs.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.type = "video/mp4";
+				contentFrame.src = "vhs.mp4";
 				title = "";
 				txt = "Song: Theme From Ernest Borgnine by Squarepusher";
 				break;
 			case 8:
 				modal.style.display = "block";
-				videoFrame.src = "fard.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.type = "video/mp4";
+				contentFrame.src = "fard.mp4";
 				title = "A Small Exchange";
 				txt = "<br/> Song: I'm Hungryyy... (Kirby 64)";
 				break;
 			case 9:
 				modal.style.display = "block";
-				videoFrame.src = "tennis.mp4";
+				contentFrame.className = "videoFrame";
+				contentFrame.type = "video/mp4";
+				contentFrame.src = "tennis.mp4";
 				title = "Tennis Guy";
 				txt = "<br/> Song: Parking Lot by me!!";
 				break;
+			case 10:
+				modal.style.display = "block";
+				modalContent.style.backgroundImage = "url(flowers.jpg)"
+				contentFrame.className = "audioFrame";
+				contentFrame.type = "audio/mpeg";
+				contentFrame.src = "test.mp3";
+				title = "test song";
+				txt = "<br/> Song: test";
+				break;
 			default:
 				modal.style.display = "none";
-				videoFrame.src = "none";
+				contentFrame.src = "none";
 				modalOverlay.style.zIndex = "0";
-				//videoFrame.contentWindow.postMessage('{"event":"command","func":"' + 'stopvideo' + '","args":""}', '*');
+				//contentFrame.contentWindow.postMessage('{"event":"command","func":"' + 'stopvideo' + '","args":""}', '*');
 				itmi = 0;
 		}
 		
@@ -463,7 +518,7 @@ function step(){
 	
 	//tween scroll
 	if (scrollPos < scrollTgt-.1 || scrollPos > scrollTgt+.1){
-		scrollTgt = clamp(scrollTgt, 0 - ($(modalInfo).outerHeight(true) - $(videoFrame).outerHeight(true))/$(videoFrame).outerHeight(true)*100, 100);
+		scrollTgt = clamp(scrollTgt, 0 - ($(modalInfo).outerHeight(true) - $(modalContent).outerHeight(true))/$(modalContent).outerHeight(true)*100, 100);
 		if (!mobile){scrollPos += (scrollTgt-scrollPos)/10;}
 		else{scrollPos += (scrollTgt-scrollPos)/2;}
 		modalInfo.style.top = scrollPos + "%";
