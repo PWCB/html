feeff930477afafd6e70fb4f7a177e3d014d348e
PWCB <pwcbrock@gmail.com>
2020-01-31 14:24:25 -0700

    redirect

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 ﻿if (window.location.href== "http://178.128.11.125/") {
    window.location.href = 'http://www.nfl.com/'; 
-}
+}else{
 
 var 
 mobile = 0,
@@ -762,5 +762,6 @@ function step(){
 			//do any reordering then...
 			//open all
 			//console.log(grid);
+}
 			
 }
