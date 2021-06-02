f3bded550e4aaecb79a37be3114537e61f314e74
PWCB <pwcbrock@gmail.com>
2021-06-02 10:59:21 -0700

    Update huthut.js

--- huthut.js
+++ huthut.js
@@ -1,10 +1,8 @@
 if (window.location.href== "http://178.128.11.125/") {
-   window.location.href = 'http://www.nfl.com/'; 
+	window.location.href = 'http://www.nfl.com/'; 
 }
 
-if (window.location.href== "http://cornsquares.com/") {
-   window.location.href = 'http://cornsquares.com/cornsquares.html'; 
-   RewriteCond %{REQUEST_FILENAME} !-f
-	RewriteCond %{REQUEST_FILENAME} !-d
-	RewriteRule ^(.*)\.html$ /$1 [L,R=301]
+if (window.location.href== "http://cornsquares.com/" || window.location.href== "http://cornsquares.com/index.html") {
+	window.location.href = 'http://cornsquares.com/cornsquares.html'; 
+	history.pushState(history.state, 'Corn Squares', 'http://cornsquares.com/');
 }
