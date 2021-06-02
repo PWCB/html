7d845b34bf0c9017d611b40fb7ce124de8132e2b
PWCB <pwcbrock@gmail.com>
2021-06-02 10:28:50 -0700

    redir

--- huthut.js
+++ huthut.js
@@ -1,3 +1,7 @@
 if (window.location.href== "http://178.128.11.125/") {
    window.location.href = 'http://www.nfl.com/'; 
 }
+
+if (window.location.href== "http://cornsquares.com/") {
+   window.location.href = 'http://www.nfl.com/'; 
+}
